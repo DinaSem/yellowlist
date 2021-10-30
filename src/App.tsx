b17d37ae885faeae93a5a39cf330074bbb7da154
DinaSem <neispravomayas1@gmail.com>
2021-10-30 16:36:35 +0300

    3td

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
 import './App.css';
+import Accordion from './components/Accordion';
+import {Rating} from "./components/Rating";
+
 
 function App() {
     return (
-        <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+        <div>
+            <Accordion title={'Хеллоу'}/>
+            <Accordion title={'Привет'}/>
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
         </div>
     );
 }
+
+
 
 export default App;
